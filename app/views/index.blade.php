bdaf0e91d025c9b930ed9069f1ca8ea8f52c918f
Joe Torraca <joe@joetorraca.com>
2014-08-23 10:01:51 -0400

    Realized the name is AnyYak, not AnyZip

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -8,7 +8,7 @@ AnyYak
 <div class="header col-md-12">
 	<div class="container">
 		<div class="col-md-4"></div>
-		<h1 class="header text-center col-md-4 large">AnyZip</h1>
+		<h1 class="header text-center col-md-4 large">AnyYak</h1>
 		<div class="col-md-4"></div>
 	</div>
 </div>
