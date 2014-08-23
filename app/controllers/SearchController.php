bbf7b70a562d8d1e44e7b6f0d7aa60c678a300de
Joe Torraca <joe@joetorraca.com>
2014-08-23 10:20:07 -0400

    Added zipcode search. Now gets lat/long (I think, cant really test it because y'know, 34,000 feet in the air)

--- app/controllers/SearchController.php
+++ app/controllers/SearchController.php
@@ -4,7 +4,15 @@ class SearchController extends BaseController {
 
 	public function index()
 	{
-		dd(Input::all());
+		$zipCode = Input::only('zip');
+
+		// $latLong = json_decode(file_get_contents('http://ziplocate.us/api/v1/'.$zipCode));
+
+		// Offline testing only, NOT FOR PRODUCTION
+		$latLong = array('lat' => 40.6637728307175, 'long' => -73.6379879280965, 'zip' => '11570');
+
+		
+
 	}
 
 }
