feef98821f71cd5681e21723fff8cc0ae63f83f5
Joe Torraca <joe@joetorraca.com>
2014-08-23 09:56:09 -0400

    Created initial routes and removed fluff

--- app/routes.php
+++ app/routes.php
@@ -1,17 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
+Route::get('/', ['uses' => 'HomeController@index', 'as' => 'home']);
 
-Route::get('/', function()
-{
-	return View::make('hello');
-});
+Route::post('search', ['uses' => 'SearchController@index', 'as' => 'search.index']);
