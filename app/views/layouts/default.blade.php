95c26ca9370109c2e14451558919b083f6ea97c3
Joe Torraca <joe@joetorraca.com>
2014-09-30 16:46:55 -0400

    Moved JS scripts to bottom of page

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -16,9 +16,6 @@
 			</style>
 		<![endif]-->
 		@yield('css')
-		<script src="/components/jquery/dist/jquery.min.js"></script>
-		<script src="/components/bootstrap/dist/js/bootstrap.min.js"></script>
-		@yield('js')
 	</head>
 	<body>
 		<div class="page_wrap">
@@ -38,6 +35,9 @@
 				<p class="made_by text-center" style="line-height:60px;margin:0px;">Made on a <span class="fa fa-plane"></span> out to San Francisco by <a href="https://joetorraca.com" title="Joe Torraca">Joe Torraca</a>.</p>
 			</div>
 		</footer>
+		<script src="/components/jquery/dist/jquery.min.js"></script>
+		<script src="/components/bootstrap/dist/js/bootstrap.min.js"></script>
+		@yield('js')
 		<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-54131867-1', 'auto');ga('require', 'displayfeatures');ga('send', 'pageview');</script>
 	</body>
 </html>
